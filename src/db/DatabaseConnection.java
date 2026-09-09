10f608765fbc10135c14590dc7bedea1b322ade3
3PT <3PT@B208-13>
2026-09-09 10:32:13 +0300

    Izveidots savienojums ar DB

--- src/db/DatabaseConnection.java
+++ src/db/DatabaseConnection.java
@@ -1,5 +1,22 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+	private static final String URL = 
+									"jdbc:mysql://localhost:3307/world"
+									+"?useSSL=false"
+									+"&allowPublicKeyRetrieval=true"
+									+"&serverTimezone=Europe/Riga"
+									+"&characterEncoding=utf8"
+									+"&useUnicode=true";
 	
+	private static final String USER = "root";
+	private static final String PASSWORD = "Parole1";
+	
+	public static Connection getConnection() throws SQLException{
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+	}
 }
